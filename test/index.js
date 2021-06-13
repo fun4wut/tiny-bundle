0a3e8b256e310413451c1bab95de3dcafca13cfb
fun4wut <nihouze@163.com>
2021-06-13 11:33:36 +0800

    fix: test单独用个npm

--- test/index.js
+++ test/index.js
@@ -3,5 +3,4 @@ import sum from 'lodash-es/sum'
 // import { at } from 'lodash-es'
 
 const onv = 333
-export const aaa = multiply(2, onv)
-console.log(sum[2,3,3], )
+console.log(sum[2,3,3], multiply(2, onv))
